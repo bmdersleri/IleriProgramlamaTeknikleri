445428382841d8f9621e930bbcaaacae3b892af4
CanCilek <46296419+CanCilek@users.noreply.github.com>
2021-06-06 21:10:39 +0300

    AdaptorTasarimDeseni_SeymaAltun

---
+++ AdaptorTasarimDeseni_SeymaAltun/adaptortasarimdeseni/adaptortasarimdeseni/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adaptortasarimdeseni
+{
+    //Client
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hesap Sonucu");
+            float sayi = 8.56f;
+            Hesaplama hesap = new HesaplamaToplama();
+            //hesap.DegeriHesapla(sayi);
+            Hedef adaptor = new Adaptor(hesap);
+            var sonuc = adaptor.DegeriHesapla(sayi);
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+    //Target interface
+    interface Hedef
+    {
+        int DegeriHesapla(float deger);
+    }
+    //Adapter class
+    class Adaptor : Hedef
+    {
+        Hesaplama _hesap;
+        public Adaptor(Hesaplama hesap)
+        {
+            this._hesap = hesap;
+        }
+        public int DegeriHesapla(float deger)
+        {
+            var dDeger = (int)deger;
+            return _hesap.DegeriHesapla(dDeger);
+
+        }
+    }
+    //Adaptee
+    interface Hesaplama
+    {
+        int DegeriHesapla(int deger);
+    }
+    class HesaplamaToplama : Hesaplama
+    {
+        public int DegeriHesapla(int deger)
+        {
+            return deger + deger;
+        }
+    }
+}
+
+
