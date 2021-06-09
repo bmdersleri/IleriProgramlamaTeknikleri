12ac12a7faeaf733cc298fffa68ac308c3168c1d
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-09 15:15:34 +0300

    RepositoryPattern_AlicanDursun

---
+++ RepositoryPattern_AlicanDursun/RepositoryPatternModel/vmTablolar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternModel
+{
+    public class tblKullanicilar
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string Sifre { get; set; }
+    }
+
+    public class tblDersler
+    {
+        public int Id { get; set; }
+        public string DersAdi { get; set; }
+    }
+
+    public class vmTablolar
+    {
+        public List<tblKullanicilar> glst_tblKullanicilar { get; set; }
+        public tblKullanicilar tblKullanicilar { get; set; }
+
+        public List<tblDersler> glst_tblDersler { get; set; }
+        public tblDersler tblDersler { get; set; }
+
+    }
+}
---
+++ RepositoryPattern_AlicanDursun/RepositoryPatternWork/GenericRepository.cs
@@ -0,0 +1,76 @@
+﻿using Dapper;
+using DapperExtensions;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPatternWork
+{
+    public class GenericRepository<T> : IDisposable where T : class
+    {
+        private readonly string _tabloAdi;
+        public readonly SqlConnection SQL = null;
+
+        public GenericRepository(string tabloAdi)
+        {
+            _tabloAdi = tabloAdi;
+        }
+
+        public void Dispose()
+        {
+            if (SQL != null)
+            {
+                SQL.Dispose();
+                GC.SuppressFinalize(this);
+            }
+        }
+        public SqlConnection SqlConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["SQL"].ConnectionString);
+        }
+        public IDbConnection CreateConnection()
+        {
+            var SQL = SqlConnection();
+            SQL.Open();
+            return SQL;
+        }
+
+        public async Task<List<T>> Liste_Getir()
+        {
+            using (var connection = CreateConnection())
+            {
+                return await connection.QueryAsync<T>($"SELECT * FROM {_tabloAdi}") as List<T>;
+            }
+        }
+
+        public async Task Veri_Kaydet(T t)
+        {
+            using (var connection = CreateConnection())
+            {
+                await connection.InsertAsync<T>(t);
+            }
+        }
+
+        public async Task Veri_Guncelle(T t)
+        {
+            using (var connection = CreateConnection())
+            {
+                await connection.UpdateAsync<T>(t);
+            }
+        }
+        public async Task Veri_Sil(int id)
+        {
+            using (var connection = CreateConnection())
+            {
+                await connection.ExecuteAsync($"DELETE FROM {_tabloAdi} WHERE Id=@Id", new { Id = id });
+
+            }
+        }
+
+    }
+}
