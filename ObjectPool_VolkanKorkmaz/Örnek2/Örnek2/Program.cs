a79723d8403cce931f680466062750b6226e00a4
Bilgisayar Muhendisligi Dersleri <74094387+bmdersleri@users.noreply.github.com>
2021-06-16 07:19:50 +0300

    ObjectPool_VolkanKorkmaz

---
+++ ObjectPool_VolkanKorkmaz/Örnek2/Örnek2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+
+namespace Örnek2
+{
+    class ExampleObject
+    {
+        public ExampleObject()
+        {
+            Console.WriteLine($"{nameof(ExampleObject)} nesnesi oluşturulmuştur. Id : {_id}");
+        }
+        public ExampleObject(int id)
+        {
+            _id = id;
+            Console.WriteLine($"{nameof(ExampleObject)} nesnesi oluşturulmuştur. Id : {_id}");
+        }
+
+        public void Write() => Console.WriteLine($"Id : {_id}");
+        int _id;
+
+        static void Main(string[] args)
+        {
+            DefaultObjectPoolProvider objectPoolProvider = new DefaultObjectPoolProvider();
+            ObjectPool<ExampleObject> objectPool = objectPoolProvider.Create(new DefaultPooledObjectPolicy<ExampleObject>());//Constructor parametresiz olmalı.
+            ExampleObject object1 = objectPool.Get();
+            object1.Write();
+            objectPool.Return(object1);
+
+            ExampleObject object2 = objectPool.Get();
+            object2.Write();
+            objectPool.Return(object2);
+        }
+    }
+}
